1cfb2ae041132dd56ec7569572c0bf924d683486
PennyAfNorberg <37243833+PennyAfNorberg@users.noreply.github.com>
2019-03-14 00:39:04 +0100

    just a q

---
+++ SQLQuery4.sql
@@ -0,0 +1,11 @@
+﻿select * from courses t1
+--assets\img\0_JLPdbimCEnWB8qJL.png
+alter table courses
+drop column courseImgPath
+
+
+alter table courses
+add  CourseImgPath nvarchar(max) null
+
+courseImgPath
+assets\img\0_JLPdbimCEnWB8qJL.png
